9affaf09af4de816c6aa190794927f4e3d5388d7
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-13 10:15:08 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Button";
+
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    
+
+    return (
+        <div className="App">
+            <div>COUNT</div>
+            <Button title={'inc'}/>
+            <Button title={'reset'}/>
+        </div>
+    );
 }
 
 export default App;
