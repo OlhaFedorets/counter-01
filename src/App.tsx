699d149168d95799ddd719a2879e69e3eaee29c1
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-13 10:58:48 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./Button";
 
 
 function App() {
 
-    
+    const [count, setCount] = useState<number>(0)
+
+    const increaseFunction = () => {
+        if (count < 5) {
+            setCount(count + 1)
+        }
+    }
+
+    const resetFunction = () => {
+        setCount(0)
+    }
 
     return (
         <div className="App">
-            <div>COUNT</div>
-            <Button title={'inc'}/>
-            <Button title={'reset'}/>
+            <div>{count}</div>
+            <Button title={'inc'}
+                    onClickHandler={increaseFunction}/>
+            <Button title={'reset'}
+                    onClickHandler={resetFunction}/>
         </div>
     );
 }
