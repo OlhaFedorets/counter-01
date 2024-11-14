d80d7b2d2601e7bd8c4773927254643b2e11454a
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-14 14:51:10 +0300

    change button

--- src/Button.tsx
+++ src/Button.tsx
@@ -8,10 +8,20 @@ type ButtonPropsType = {
 
 export const Button = ({title, onClickHandler}: ButtonPropsType) => {
 
-
     return (
-        <button onClick={onClickHandler}>{title}</button>
+        <StyledButton onClick={onClickHandler}>{title}</StyledButton>
     );
 };
 
+
+
+const StyledButton = styled.button`
+    margin: 20px 0 0 20px;
+    width: 120px;
+    height: 40px;
+    background-color: #47a5af;
+    border: none;
+    border-radius: 10px;
+    font-size: 16px;
+`
 
